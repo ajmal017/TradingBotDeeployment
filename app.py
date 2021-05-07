b8c6cfca7aa6c014789a55173ea6965d47dce76d
Sagar Sanjay Bhalke <sagarbhalke888@gmail.com>
2021-05-07 18:42:04 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, abort
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return "Hello Deployemnt Working"
+
+
+@app.route('/hit')
+def hit():
+    return "Hello Tradingview Hit"
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        print(request.json)
+        return '', 200
+    else:
+        abort(400)
+
+
+if __name__ == '__main__':
+    app.run()
