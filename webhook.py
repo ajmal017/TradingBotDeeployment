a60f31e10f34b749213f28568f732673541efbc2
Sagar Sanjay Bhalke <sagarbhalke888@gmail.com>
2021-05-07 18:37:00 +0530

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -3,6 +3,15 @@ from flask import Flask, request, abort
 app = Flask(__name__)
 
 
+@app.route('/')
+def home():
+    return "Hello Deployemnt Working"
+
+
+@app.route('/hit')
+def hit():
+    return "Hello Tradingview Hit"
+
 @app.route('/webhook', methods=['POST'])
 def webhook():
     if request.method == 'POST':
