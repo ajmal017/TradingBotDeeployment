5bc632bdaf258654e9c1d77092bf6ed8d3e2cbd4
Sagar Sanjay Bhalke <sagarbhalke888@gmail.com>
2021-05-08 00:37:51 +0530

    data

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def hit():
 def webhook():
     if request.method == 'POST':
         print("*****************************************************")
-        print(request)
+        print(request.data)
         print("*****************************************************")
         return '', 200
     else:
