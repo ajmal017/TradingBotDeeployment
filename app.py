cb6cd745f4dec3531f320894d9d7c190737b12f8
Sagar Sanjay Bhalke <sagarbhalke888@gmail.com>
2021-05-11 09:37:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 kite = KiteConnect(api_key="60sdy72jltn7a949")
 
-data = kite.generate_session("NsFuRxets9IP51wvXqb2fF7YT5kB4Gwt", api_secret="cbjxfw3tek45p7thq3yywyy5gsvuc4t8")
+data = kite.generate_session("bK0GTX4f870tvVCgykCsZn2zbKVoeeVz", api_secret="cbjxfw3tek45p7thq3yywyy5gsvuc4t8")
 kite.set_access_token(data["access_token"])
 
 @app.route('/')
